2ca1642180d48cd4301c63fb121611b4be24c17b
swobenator <148558624+swobenator@users.noreply.github.com>
2025-02-23 18:19:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-Window.size =(400, 720)
-
-
 
 class grid(GridLayout):
     def __init__(self, **kwargs):
